376eb7034a861e6c21df67628c37f4369aa4b03c
Netanel Lusky <natilusky@gmail.com>
2023-04-16 11:36:01 +0300

    error fixed

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import path from 'path';
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-// Load environment variables
-dotenv.config();
+dotenv.config()
 
-// Create an instance of the express application
 const app = express();
 
-// Apply middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'static')));
 
-// Define routes
-app.get('/el-al', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'el-al', 'index.html'));
-});
+app.use(express.static('./static'));
 
-app.get('/blue', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'blue', 'index.html'));
-});
+app.get('/el-al', (req, res) => res.sendFile(path.join(__dirname, 'static', 'el-al', 'index.html')));
+app.get('/blue', (req, res) => res.sendFile(path.join(__dirname, 'static', 'blue', 'index.html')));
+app.get('/beauty-care', (req, res) => res.sendFile(path.join(__dirname, 'static', 'blue', 'form.html')));
 
-app.get('/beauty-care', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'blue', 'form.html'));
-});
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
