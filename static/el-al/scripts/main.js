9aedb41ec98f8a2c0b371e0aaa7a4ac886669269
Netanel Lusky <natilusky@gmail.com>
2023-06-15 13:57:48 +0300

    el-al link fixed

--- static/el-al/scripts/main.js
+++ static/el-al/scripts/main.js
@@ -1,16 +1,16 @@
 
-$(document).ready(function(){
-  $('.loader').css("display","none")
+$(document).ready(function () {
+  $('.loader').css("display", "none")
   $(".alert").hide();
 
-  $( "#flight-date" ).datepicker({
-    monthNames: ['ינואר','פברואר','מרץ','אפריל','מאי','יוני',
-    'יולי','אוגוסט','ספטמבר','אוקטובר','נובמבר','דצמבר'],
-    monthNamesShort: ['ינו','פבר','מרץ','אפר','מאי','יוני',
-    'יולי','אוג','ספט','אוק','נוב','דצמ'],
-    dayNames: ['ראשון','שני','שלישי','רביעי','חמישי','שישי','שבת'],
-    dayNamesShort: ['א\'','ב\'','ג\'','ד\'','ה\'','ו\'','שבת'],
-    dayNamesMin: ['א\'','ב\'','ג\'','ד\'','ה\'','ו\'','שבת'],
+  $("#flight-date").datepicker({
+    monthNames: ['ינואר', 'פברואר', 'מרץ', 'אפריל', 'מאי', 'יוני',
+      'יולי', 'אוגוסט', 'ספטמבר', 'אוקטובר', 'נובמבר', 'דצמבר'],
+    monthNamesShort: ['ינו', 'פבר', 'מרץ', 'אפר', 'מאי', 'יוני',
+      'יולי', 'אוג', 'ספט', 'אוק', 'נוב', 'דצמ'],
+    dayNames: ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת'],
+    dayNamesShort: ['א\'', 'ב\'', 'ג\'', 'ד\'', 'ה\'', 'ו\'', 'שבת'],
+    dayNamesMin: ['א\'', 'ב\'', 'ג\'', 'ד\'', 'ה\'', 'ו\'', 'שבת'],
     dateFormat: 'dd/mm/yy',
     isRTL: true,
     changeYear: true,
@@ -21,74 +21,74 @@ $(document).ready(function(){
 
   const getUrlParameter = (sParam) => {
     let sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
     for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
+      sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
     }
     return false;
   };
 
-  $( "#submitbtn" ).click(function() {
+  $("#submitbtn").click(function () {
     $(".alert").hide();
-    $('.loader').css("display","block")
-    
-    if($("#flight-date").val()=="" || $("#booking-number").val()=="" || $("#reason-for-contact").val()=="" || $("#passenger-name").val()=="" || $("#phone").val()==""){
+    $('.loader').css("display", "block")
+
+    if ($("#flight-date").val() == "" || $("#booking-number").val() == "" || $("#reason-for-contact").val() == "" || $("#passenger-name").val() == "" || $("#phone").val() == "") {
       var errorMessage = "נא למלא את כל השדות"
       $('.alert-danger').html(errorMessage)
       $('.alert-danger').show();
-      $('.loader').css("display","none")
-      setTimeout(function(){$(".alert").hide();}, 3000); 
-    }else{
+      $('.loader').css("display", "none")
+      setTimeout(function () { $(".alert").hide(); }, 3000);
+    } else {
       const ticketIdVal = getUrlParameter('ticketId')
       const dataFlight = {
-        ticketId : ticketIdVal,
-        phoneNumber : $("#phone").val(),
-        field2 : $("#booking-number").val(),
-        field4 : $("#reason-for-contact").val(),
-        field6 : $("#passenger-name").val(),
-        field7 : $("#flight-date").val()
+        ticketId: ticketIdVal,
+        phoneNumber: $("#phone").val(),
+        field2: $("#booking-number").val(),
+        field4: $("#reason-for-contact").val(),
+        field6: $("#passenger-name").val(),
+        field7: $("#flight-date").val()
       }
       console.log(JSON.stringify(dataFlight));
       $.ajax({
-        url: `api/send-form`,
+        url: `https://glassix.consist.co.il/el-al/api/send-form`,
         type: 'post',
         dataType: 'json',
         contentType: 'application/json',
         data: JSON.stringify(dataFlight),
         success: function (data) {
-          console.log("data "+data);
-          if(!success){
-              $('.alert-danger').text('אירע שגיאה');
-              $('.alert-danger').show();
-              $('.loader').css("display","none") 
-          }
-          else{
-            $('.alert-success').show();
-            $('.loader').css("display","none") 
-          }
-        },
-        error:function (data) {
-          $('.alert-danger').text('אירע שגיאה');
-          $('.alert-danger').show();
-          $('.loader').css("display","none") 
-        },
-        complete: function(data){
-          if(!success){
+          console.log("data " + data);
+          if (!success) {
             $('.alert-danger').text('אירע שגיאה');
             $('.alert-danger').show();
-            $('.loader').css("display","none") 
+            $('.loader').css("display", "none")
           }
-          else{
+          else {
             $('.alert-success').show();
-            $('.loader').css("display","none") 
+            $('.loader').css("display", "none")
           }
-          $('.loader').css("display","none")
-        }  
+        },
+        error: function (data) {
+          $('.alert-danger').text('אירע שגיאה');
+          $('.alert-danger').show();
+          $('.loader').css("display", "none")
+        },
+        complete: function (data) {
+          if (!success) {
+            $('.alert-danger').text('אירע שגיאה');
+            $('.alert-danger').show();
+            $('.loader').css("display", "none")
+          }
+          else {
+            $('.alert-success').show();
+            $('.loader').css("display", "none")
+          }
+          $('.loader').css("display", "none")
+        }
       })
     }
   })
