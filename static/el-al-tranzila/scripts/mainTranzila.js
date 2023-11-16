892736804bc9cb1cb8dd3892ee442fa1eb11f503
Netanel Lusky <netanell@consist-sys.consist.co.il>
2023-11-16 17:02:06 +0200

    make the font bigger and change coin display

--- static/el-al-tranzila/scripts/mainTranzila.js
+++ static/el-al-tranzila/scripts/mainTranzila.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  $("#creatTicketForm").hide()
+  $("#creatTicketForm").hide();
   // Get the ticketId from the URL.
   const ticketId = getTicketIdFromUrl();
 
-  $("#submitbtn").click(function(){
-    if($("#approve").is(':checked')){
-      $("#iframe1").hide()
-      $("#creatTicketForm").show()
+  $("#submitbtn").click(function () {
+    if ($("#approve").is(":checked")) {
+      $("#iframe1").hide();
+      $("#creatTicketForm").show();
     }
   });
 
@@ -14,44 +14,50 @@ $(document).ready(function () {
   $.ajax({
     url: `https://glassix.consist.co.il/el-al/api/get-data/${ticketId}`,
     type: "GET",
-    success: function(data) {
+    success: function (data) {
       // The data from the API includes all necessary fields like clientName, PNR, etc.
-      handleApiResponse(data)
-      $(".noIframeT").hide()
-      $(".IframeT").show()
+      handleApiResponse(data);
+      $(".noIframeT").hide();
+      $(".IframeT").show();
     },
-    error: function(err) {
+    error: function (err) {
       // Handle error.
       console.log(err);
       $(".IframeT").hide();
       $(".noIframeT").html("הדף כבר לא זמין, אנא דבר שוב עם הנציג").show();
-    }
+    },
   });
-
 });
 
 function getTicketIdFromUrl() {
   // Split the URL by '/' and get the last part
-  const urlParts = window.location.pathname.split('/');
+  const urlParts = window.location.pathname.split("/");
   return urlParts[urlParts.length - 1];
 }
 
+const getCurrency = (currency) => {
+  switch (currency) {
+    case "1":
+    case 1:
+      return "שקל";
+    case "2":
+    case 2:
+      return "דולר";
+    case "978":
+    case 978:
+      return "יורו";
+    default:
+      return "Unknown Currency"; // handle unknown or default case
+  }
+};
+
 function handleApiResponse(data) {
   // Destructure the data.
-  const {
-    clientName,
-    PNR,
-    currency,
-    sum,
-    userName,
-    departmentId,
-    expiration,
-    ticketId,
-  } = data;
+  const { clientName, PNR, currency, sum, userName, departmentId, expiration, ticketId } = data;
 
   // Get additional parameters.
-  const cred_type = '1';
-  const tranmode = 'VK';
+  const cred_type = "1";
+  const tranmode = "VK";
 
   // Create the source URL for the iframe.
   const src = `https://direct.tranzila.com/consistelal/iframenew.php?nologo=1&clientName=${clientName}&PNR=${PNR}&expiration=${expiration}&userName=${userName}&sum=1&currency=${currency}&cred_type=${cred_type}&tranmode=${tranmode}&lang=il&ticketId=${ticketId}&departmentId=${departmentId}&tahles=${sum}&hidesum=1`;
@@ -59,10 +65,10 @@ function handleApiResponse(data) {
   // Set the source of the iframe.
   $("#iframeTrzanzila").prop("src", `${src}`);
   $(".noIframeT").hide();
-  $(".textClientName").html(clientName)
-  $(".textPNR").html(PNR)
-  $(".textCoin").html(currency)
-  $(".textSum").html(sum)
+  $(".textClientName").html(clientName);
+  $(".textPNR").html(PNR);
+  $(".textCoin").html(getCurrency(currency));
+  $(".textSum").html(sum);
   // // Handle the expiration.
   // if (Date.now() < expiration) {
   //   $(".IframeT").hide();
